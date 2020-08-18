aa2b2d5eb0aa79ee0a5f43faa30435507f5c21e7
Jose Cazarin <joseespiriki@gmail.com>
2020-08-17 22:33:10 -0600

    adding serial port code

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+import serial
+
+def get_value():
+
+    ser = serial.Serial('COM5')
+
+    data = ser.read(10).decode()
+    ser.close()
+
+    idx = data.index('.')
+
+    if idx == 0 :
+        integer_part = "0"
+    else:
+        integer_part = data[idx - 1]
+
+    decimal_part = data[idx + 1: idx + 3]
+
+    whole_data = integer_part + "." + decimal_part
+
+    if whole_data[-1] == '.':
+        whole_data = whole_data[:-1]
+
+    return whole_data
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return HttpResponse("Value read from serial port = " + str(get_value()))
